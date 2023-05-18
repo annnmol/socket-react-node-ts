39ca4b4e47f8d98da732878ef240cbcde23252b4
anmoltanwar <anmol.tanwar@geta.ai>
2023-05-18 16:21:10 +0530

    inital commit

---
+++ socket-backend/src/utils/Enum.ts
@@ -0,0 +1,9 @@
+export enum SOCKET_CONNECTION_TYPES {
+    SEND_MESSAGE = 'message',
+    RECEIVE_MESSAGE = 'receive_message',
+    JOIN_ROOM = 'join_room',
+    CONNECT = 'connect',
+    DISCONNECT = 'disconnect',
+    AGENT_CONNECT = 'agent_connect',
+    CLIENT_CONNECT = 'client_connect',
+}
