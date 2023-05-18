241e53ee03474fa1c0349a70a1a6f20a2f15fe00
anmoltanwar <anmol.tanwar@geta.ai>
2023-05-18 16:23:30 +0530

    inti

--- socket-backend/src/utils/Enum.ts
+++
@@ -1,9 +0,0 @@
-export enum SOCKET_CONNECTION_TYPES {
-    SEND_MESSAGE = 'message',
-    RECEIVE_MESSAGE = 'receive_message',
-    JOIN_ROOM = 'join_room',
-    CONNECT = 'connect',
-    DISCONNECT = 'disconnect',
-    AGENT_CONNECT = 'agent_connect',
-    CLIENT_CONNECT = 'client_connect',
-}
